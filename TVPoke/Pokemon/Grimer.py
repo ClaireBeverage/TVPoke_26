491847beb4a43c6f2f0107d7600ec832ba266f68
ClaireBeverage <claire@justlikeadrink.com>
2026-03-05 10:13:10 -0700

    i made my pokemon

--- TVPoke/Pokemon/Grimer.py
+++ TVPoke/Pokemon/Grimer.py
@@ -4,9 +4,9 @@ from TVPoke.BaseClasses.Move import Move
 class Golduck(Poison):
     def __init__(self):
         moves = [
-            Move("Crunch", "", 40),
-            Move("Poison Jab", "", 40),
-            Move("Bite", "", 80),
-            Move("Mud Bomb", "", 0)
+            Move("Crunch", "NORMAL", 40),
+            Move("Poison Jab", "POISON", 40),
+            Move("Acid Armor", "POISON", 80),
+            Move("Mud Bomb", "GROUND", 0)
         ]
         super().__init__("Grimer", 80, moves, "./TVPoke/Pokemon/imgs/Grimer.png")
